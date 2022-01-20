52d8aa5e6ed49f1c319ff4de9f9cf81c5019846a
Cai-Liming <cai.liming@iisgalvanimi.edu.it>
2022-01-20 21:31:02 +0000

    1

---
+++ animali.js
@@ -0,0 +1,11 @@
+export default class animali{
+    tipo;
+    razza;
+    nzampe;
+
+    constructor(t, r, n){
+        this.tipo=t;
+        this.razza=r;
+        this.nzampe=n;
+    }
+};
---
+++ script.js
@@ -0,0 +1,5 @@
+import animali from "./animali";
+
+export function mucca(){
+     var mu = new animali("mucca"+ "mucca"+"4" )
+}
